371529df2ef878efa85fda96ed6d5b098308a77b
adrixo <adrianvalrom@gmail.com>
2022-10-12 16:23:20 +0200

    test(red): add parameterized test for step1O

--- src/test/java/stringcalculator/StringCalculatorShould.java
+++ src/test/java/stringcalculator/StringCalculatorShould.java
@@ -3,6 +3,8 @@ package stringcalculator;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,9 +18,16 @@ class StringCalculatorShould {
         calculator = new StringCalculator();
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({
+            "'1,2', 3",
+            "'', 0",
+            "'4', 4",
+            "'-1,2', 1",
+            "'-1,-2', -3",
+    })
     public void
-    step_1_add_two_separated_numbers() {
-        assertThat(calculator.add("1,2")).isEqualTo(3);
+    step_1_add_two_separated_numbers(String input, int expectedResult) {
+        assertThat(calculator.add(input)).isEqualTo(expectedResult);
     }
 }
