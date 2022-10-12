a5f17328d82a90ac711d5c2bbd434d5a6dbede1b
adrixo <adrianvalrom@gmail.com>
2022-10-12 19:01:44 +0200

    test(green): step 7 add custom long separators

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -2,6 +2,7 @@ package stringcalculator;
 
 public class StringCalculator {
 
+    private static String character;
     private String separators = ",|\n";
 
     public int add(String input) throws NegativesNotAllowed, NumberTooBig {
@@ -18,8 +19,15 @@ public class StringCalculator {
     }
 
     private String[] getNumberList(String input) {
+        // step 7 also forced to change this function
         if (hasCustomSeparator(input)) {
-            input = input.substring(3);
+            int longSeparatorIndex = input.indexOf("]");
+            if (longSeparatorIndex == -1)
+                input = input.substring(3);
+            else {
+                longSeparatorIndex++;
+                input = input.substring(longSeparatorIndex);
+            }
         }
         return input.split(separators);
     }
@@ -30,7 +38,20 @@ public class StringCalculator {
     }
 
     private static String getCustomSeparator(String input) {
-        return String.valueOf(input.charAt(2));
+        character = String.valueOf(input.charAt(2));
+        String separator = null;
+        // KLDG: Coupling between split/getCustomSeparator
+        // due to split logic and new feature
+        // (modify this logic requires split knowledge)
+        if (character.equals("[")) {
+            int separatorClose = input.indexOf("]");
+            separatorClose++;
+            // even if we have same logic using []...
+            separator = "["+ input.substring(2, separatorClose) +"]";
+        } else {
+            separator = character;
+        }
+        return separator;
     }
 
     private static boolean hasCustomSeparator(String input) {
@@ -44,7 +65,10 @@ public class StringCalculator {
     private static int sum(String[] stringInput) throws NegativesNotAllowed, NumberTooBig {
         int result = 0;
         for(String strNumber : stringInput) {
-            int number = Integer.parseInt(strNumber);
+            // also the coupling at step 7 forced me to change "" behaviour
+            int number = 0;
+            if (!strNumber.equals(""))
+                number = Integer.parseInt(strNumber);
             validateNumber(number);
             result += number;
         }
