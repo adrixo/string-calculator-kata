b6707c4286ce9c3cd3e7a8333634edb10b6b1b7f
adrixo <adrianvalrom@gmail.com>
2022-10-12 16:35:59 +0200

    test: green parametized test with step 1

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -2,8 +2,16 @@ package stringcalculator;
 
 public class StringCalculator {
 
-    public int add(String s) {
-        return 3;
+    public int add(String input) {
+        String[] stringInput = input.split(",");
+        if(stringInput[0].equals("")) {
+            return 0;
+        }
+        int result = 0;
+        for(String number : stringInput) {
+            result += Integer.parseInt(number);
+        }
+        return result;
     }
 
 }
