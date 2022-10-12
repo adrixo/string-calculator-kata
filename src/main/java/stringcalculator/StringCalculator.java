4de0dabe1f770a11395ad804e6f213e0266f0829
adrixo <adrianvalrom@gmail.com>
2022-10-12 17:40:19 +0200

    refactor: simplify add code

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -2,16 +2,46 @@ package stringcalculator;
 
 public class StringCalculator {
 
+    private String separators = ",|\n";
+
     public int add(String input) {
-        String separators = ",|\n";
-        if (input.startsWith("//")) {
-            separators += "|" + input.charAt(2);
+        if (hasCustomSeparator(input)) {
+            addSeparator(input);
+        }
+
+        String[] numbers = getNumberList(input);
+
+        if(isEmpty(numbers))
+            return 0;
+
+        return sum(numbers);
+    }
+
+    private String[] getNumberList(String input) {
+        if (hasCustomSeparator(input)) {
             input = input.substring(3);
         }
-        String[] stringInput = input.split(separators);
-        if(stringInput[0].equals("")) {
-            return 0;
-        }
+        return input.split(separators);
+    }
+
+    private void addSeparator(String input) {
+        String customSeparator = getCustomSeparator(input);
+        separators += "|" + customSeparator;
+    }
+
+    private static String getCustomSeparator(String input) {
+        return String.valueOf(input.charAt(2));
+    }
+
+    private static boolean hasCustomSeparator(String input) {
+        return input.startsWith("//");
+    }
+
+    private static boolean isEmpty(String[] stringInput) {
+        return stringInput[0].equals("");
+    }
+
+    private static int sum(String[] stringInput) {
         int result = 0;
         for(String number : stringInput) {
             result += Integer.parseInt(number);
